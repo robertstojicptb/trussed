f966949c03c71c450c63044e1b2c176b92ca616d
Jan Nordholz <j.nordholz@tu-berlin.de>
2022-08-29 03:11:53 +0200

    Add P256 key generation for SE050 backend.

--- src/service/backend_se050.rs
+++ src/service/backend_se050.rs
@@ -3,6 +3,8 @@ use crate::error::{Error, Result};
 use crate::types::*;
 use se050::Se050Device;
 
+const SE050_ID_SPACE: u32 = 0x53453530;	 /* 'SE50' */
+
 pub struct Se050Wrapper {
     pub device: &'static mut dyn Se050Device,
     pub delay: &'static mut se050::DelayWrapper,
@@ -32,6 +34,15 @@ impl ServiceBackend for Se050Wrapper {
 				Err(Error::RequestNotAvailable)
 			}
 		},
+		Request::GenerateKey(request) => {
+			match request.mechanism {
+			Mechanism::P256 => {
+				let objid = self.device.generate_p256_key(self.delay).unwrap();
+				Ok(Reply::GenerateKey(reply::GenerateKey { key: KeyId(objid.into()) }))
+			}
+			_ => { Err(Error::RequestNotAvailable) }
+			}
+		},
 		_ => {
 			Err(Error::RequestNotAvailable)
 		}
@@ -49,4 +60,30 @@ fn aes_encrypt() -> Result<reply::Encrypt> {
 		// 3d. use CryptoObject and SymmKey to perform computation
 		// 3e. delete transient SymmKey
 	Err(Error::RequestNotAvailable)
+}
+
+impl Into<Id> for se050::ObjectId {
+	fn into(self) -> Id {
+		Id((SE050_ID_SPACE as u128) << 96 |
+			((self.0[0] as u128) << 24) |
+			((self.0[1] as u128) << 16) |
+			((self.0[2] as u128) << 8) |
+			 (self.0[3] as u128))
+	}
+}
+
+impl TryFrom<Id> for se050::ObjectId {
+	type Error = crate::error::Error;
+
+	fn try_from(id: Id) -> Result<Self> {
+		if id.0 >> 96 != (SE050_ID_SPACE as u128) {
+			return Err(crate::error::Error::InternalError);
+		}
+		let buf: [u8; 4] = [
+			((id.0 >> 24) & 0xff) as u8,
+			((id.0 >> 16) & 0xff) as u8,
+			((id.0 >>  8) & 0xff) as u8,
+			( id.0        & 0xff) as u8];
+		Ok(Self(buf))
+	}
 }
