17341eaec3d0f5c6df148418f72b986e55db5ef8
Robert <robert.stojic@ptb.de>
2023-03-16 13:59:12 +0100

    var

--- src/service/backend_se050.rs
+++ src/service/backend_se050.rs
@@ -53,7 +53,7 @@ impl ServiceBackend for Se050Wrapper {
 			match request.mechanism {
 			Mechanism::P256 => {
 				let objid_2 = self.device.generate_p256_key(self.delay).unwrap();
-				Ok(Reply::GenerateKey(reply::GenerateKey { key: KeyId(ob_2jid.into()) }))
+				Ok(Reply::GenerateKey(reply::GenerateKey { key: KeyId(objid_2.into()) }))
 			}
 			_ => { Err(Error::RequestNotAvailable) }
 			}
