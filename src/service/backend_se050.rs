9e14e058f97c2e5ed29499d875d1847892f3bda4
Robert <robert.stojic@ptb.de>
2023-03-30 14:06:15 +0200

    var

--- src/service/backend_se050.rs
+++ src/service/backend_se050.rs
@@ -137,7 +137,7 @@ impl ServiceBackend for Se050Wrapper {
    else if request.count == 100 {
 
 	let mut bytes = Message::new();
-	 bytes.resize_default(65).unwrap(); //SLICE LENGTH 32 da 4byte		
+	 bytes.resize_default(65); //SLICE LENGTH 32 da 4byte		
 	//bytes.resize_default(request.count).unwrap();
 
    self.device.read_secure_object(&mut bytes,&[0x20, 0xe8, 0xa1, 0x01], self.delay,); 
