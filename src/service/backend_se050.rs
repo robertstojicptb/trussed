52e8bcd90681c159be1d0864aa0d2a00192104ef
Robert <robert.stojic@ptb.de>
2023-03-30 08:08:05 +0200

    var

--- src/service/backend_se050.rs
+++ src/service/backend_se050.rs
@@ -90,7 +90,7 @@ impl ServiceBackend for Se050Wrapper {
 				 let mut bytes = Message::new();
 				 bytes.resize_default(request.count).unwrap();				
 				self.device.check_object_exists(&mut bytes,&[0x20, 0xe8, 0xa1, 0x01], self.delay,);
-				self.device.check_object_exists(&mut bytes,&[0x20, 0xe8, 0xa1, 0x02], self.delay,);
+				//self.device.check_object_exists(&mut bytes,&[0x20, 0xe8, 0xa1, 0x02], self.delay,);
 				Ok(Reply::RandomBytes(reply::RandomBytes { bytes } ))
 			}
 	/*  
